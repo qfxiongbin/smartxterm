62d95940775ae7bdebc3475448072a7446f5e335
qfxiongbin <qfxiongbin@163.com>
2024-01-25 17:40:45 +0800

    Refactor App.vue component to use SmartTerminal component

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,18 @@
 <template>
   <div id="app">
-    <div 
-    ref="terminal" 
-    class="terminal" 
-    :id="'terminal'+ id"
-    :style="{height: 'calc(100vh - 30px)'}"
-    ></div>
+    <smart-terminal :terminalId="1"></smart-terminal>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import { Terminal } from 'xterm';
-import { ipcRenderer } from 'electron';
-import 'xterm/css/xterm.css';
-import { FitAddon } from 'xterm-addon-fit';
+import SmartTerminal from './components/SmartTerminal';
 
 export default {
+  name: 'App',
+  components: {
+    SmartTerminal
+  },
   setup() {
-    const xterm = ref(null);
-    const id = ref(1);
-    const fitAddon = ref(null);
-    const channels = ref(null);
-
-    const initConnect = () => {
-      destoryTerm();
-      ipcRenderer.invoke('terminal-create').then(res => {
-        let pid = res;
-        xterm.value = new Terminal();
-        fitAddon.value = new FitAddon();
-        xterm.value.loadAddon(fitAddon.value);
-        xterm.value.open(document.getElementById('terminal' + id.value));
-        channels.value = ["terminal-incomingData-" + pid, "terminal-keystroke-" + pid, "terminal-resize-" + pid, "terminal-close-" + pid];
-        xterm.value.onData((data) => {
-          ipcRenderer.send(channels.value[1], data);
-        })
-        xterm.value.onResize((size) => {
-          ipcRenderer.send(channels.value[2], size.cols, size.rows);
-        })
-        ipcRenderer.on(channels.value[0], (event, data) => {
-          xterm.value.write(data);
-        });
-        window.onresize = function() {
-          fitSize();
-        }
-        fitSize();
-        xterm.value.focus();
-      })
-    };
-
-    const destoryTerm = () => {
-      if (xterm.value) {
-        xterm.value.dispose();
-        xterm.value = null;
-      }
-      if (fitAddon.value) {
-        fitAddon.value.dispose();
-        fitAddon.value = null;
-      }
-      if (channels.value) {
-        ipcRenderer.send(channels.value[3]);
-        ipcRenderer.removeAllListeners(channels.value[0]);
-        channels.value = null;
-      }
-    };
-
-    const fitSize = () => {
-      if (fitAddon.value) {
-        fitAddon.value.fit();
-        ipcRenderer.send(channels.value[2], fitAddon.value.proposeDimensions().cols, fitAddon.value.proposeDimensions().rows);
-      }
-    };
-
-    onMounted(() => {
-      initConnect();
-    });
-
-    return {
-      xterm,
-      id,
-      fitAddon,
-      channels,
-      initConnect,
-      destoryTerm
-    };
   }
 };
 </script>
